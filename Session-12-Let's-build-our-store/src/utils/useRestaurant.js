4a08f21c2c15f0dd93a9415f7034e8156c9cb01c
Tania <taniamollah15@gmail.com>
2023-02-07 01:38:16 +0530

    commiting codes before experimenting cartSlice

--- Session-12-Let's-build-our-store/src/utils/useRestaurant.js
+++ Session-12-Let's-build-our-store/src/utils/useRestaurant.js
@@ -1,8 +1,11 @@
 import { useState, useEffect } from "react";
 import { FETCH_URL } from "../constants";
+import { useDispatch } from "react-redux";
+import { setRestaurantDetails } from "../utils/cartSlice";
 
 const useRestaurant = (resId) => {
     const [restaurant, setRestaurant] = useState(null);
+    const dispatch = useDispatch();
 
     useEffect(() => {
         getRestaurantDetails();
@@ -12,6 +15,7 @@ const useRestaurant = (resId) => {
         const data = await fetch(FETCH_URL.GET_RESTAURANT_MENU + resId);
         const json = await data.json();
         setRestaurant(json?.data)
+        /* {dispatch(setRestaurantDetails({resName: json?.data?.name, resArea: json?.data?.area, resImage: json?.data?.cloudinaryImageId}))} */
     }
     return restaurant;
 }
